1769e4450068e58754a7d711bd90c325d09b36ca
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-07-01 10:27:15 +0300

    task-2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,38 +2,15 @@ import Player from '@vimeo/player';
 const throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+player.on('play', function () {
+  const currentTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+  player.setCurrentTime(currentTime || 0);
+});
 
 player.on('timeupdate', throttle(updateTime, 1000));
 
-const pageAccessedByReload =
-  (window.performance.navigation && window.performance.navigation.type === 1) ||
-  window.performance
-    .getEntriesByType('navigation')
-    .map(nav => nav.type)
-    .includes('reload');
-
-if (pageAccessedByReload) {
-  player.on('play', function () {
-    player
-      .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-      .then(function (seconds) {
-        seconds = localStorage.getItem('videoplayer-current-time');
-        localStorage.clear();
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  });
-}
-
-function updateTime() {
-  player
-    .getCurrentTime()
-    .then(seconds => {
-      localStorage.setItem('videoplayer-current-time', seconds);
-      console.log(localStorage.getItem('videoplayer-current-time'));
-    })
-    .catch(error => {
-      console.error(error);
-    });
+function updateTime(time) {
+  console.log(time.seconds);
+  localStorage.setItem(LOCAL_STORAGE_KEY, time.seconds);
 }
